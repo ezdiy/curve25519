70163f17150ac73776e4694037e83fe6f13455ee
Fedor Indutny <fedor@indutny.com>
2017-04-30 02:38:02 -0400

    src: remove stale code

--- include/curve25519.h
+++ include/curve25519.h
@@ -3,12 +3,4 @@
 
 #include <stdint.h>
 
-typedef struct curve25519_table_s curve25519_table_t;
-
-struct curve25519_table_s {
-  uint8_t points[32][256];
-};
-
-void curve25519_precompute(curve25519_table_t* out, uint8_t point[32]);
-
 #endif  /* INCLUDE_CURVE25519_H_ */
--- src/curve25519.c
+++ src/curve25519.c
@@ -1,7 +1,4 @@
 #include "include/curve25519.h"
 
 #include "src/common.h"
-#include "src/point.h"
-
-void curve25519_precompute(curve25519_table_t* out, uint8_t point[32]) {
-}
+#include "src/edpoint.h"
